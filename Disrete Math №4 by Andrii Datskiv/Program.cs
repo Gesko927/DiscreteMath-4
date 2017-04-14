61b50ad03d4273b198cdf5941ba48508f272ebb9
Gesko927 <gesko927@gmail.com>
2017-04-14 23:50:40 +0300

    Add project files.

---
+++ Disrete Math №4 by Andrii Datskiv/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disrete_Math__4_by_Andrii_Datskiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
